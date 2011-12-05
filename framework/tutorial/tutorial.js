b0ba8dbd39ccd04d7338159bc4c64c44489550d7
Jan Miksovsky <jan@miksovsky.com>
2011-12-05 15:49:37 -0800

    Update references to rehydrate plugin to use absolute path. Start on lightweight jsFiddle-style code editor.

---
+++ framework/tutorial/tutorial.js
@@ -0,0 +1,16 @@
+//
+// CodeEditor
+//
+CodeEditor = Control.subclass({
+    name: "CodeEditor",
+    content: " "
+});
+
+//
+// TutorialPage
+//
+TutorialPage = SitePage.subclass({
+    name: "TutorialPage",
+    content: " Hello "
+});
+
