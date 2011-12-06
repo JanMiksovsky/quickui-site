3cc4d44ddae34651d4c40c53743049e01a309260
Jan Miksovsky <jan@miksovsky.com>
2011-12-05 16:48:30 -0800

    More work on tutorials' CodeEditor

--- framework/tutorial/tutorial.js
+++ framework/tutorial/tutorial.js
@@ -6,11 +6,63 @@ CodeEditor = Control.subclass({
     content: [
         " ",
         {
-            control: "BasicButton",
-            content: "Hello"
+            html: "<div />",
+            id: "CodeEditor_content"
+        },
+        " ",
+        {
+            html: "<div />",
+            id: "toolbar",
+            content: [
+                " ",
+                {
+                    control: "BasicButton",
+                    id: "buttonRun",
+                    content: "Run"
+                },
+                " "
+            ]
+        },
+        " ",
+        {
+            html: "<textarea class=\"pane\" />",
+            id: "CodeEditor_dom"
+        },
+        " ",
+        {
+            html: "<textarea class=\"pane\" />",
+            id: "CodeEditor_code"
+        },
+        " ",
+        {
+            html: "<div class=\"pane\" />",
+            id: "result"
         },
         " "
     ]
+});
+CodeEditor.prototype.extend({
+    
+    dom: Control.chain( "$CodeEditor_dom", "content" ),
+    code: Control.chain( "$CodeEditor_code", "content" ),
+    content: Control.chain( "$CodeEditor_content", "content" ),
+    
+    initialize: function() {
+        
+        var self = this;
+        this.$buttonRun().click( function() { self.run(); } );
+        
+        this.$CodeEditor_dom().content( "<div id='foo'/>")
+        this.$CodeEditor_code().content( "$('#foo').text('Hello, world');" );
+    },
+    
+    result: Control.chain( "$result", "content" ),
+    
+    run: function() {
+        this.$result().html( this.dom() );
+        eval( this.code() );
+    }
+    
 });
 
 //
@@ -19,9 +71,10 @@ CodeEditor = Control.subclass({
 TutorialPage = SitePage.subclass({
     name: "TutorialPage",
     content: [
-        " Hello ",
+        " ",
         {
-            control: "CodeEditor"
+            control: "CodeEditor",
+            content: " Here's a sample step introduction. "
         },
         " "
     ]
