329254eb42abd8828a547338c51305ba2950f0c6
Jan Miksovsky <jan@miksovsky.com>
2012-06-06 10:55:01 -0700

    Committing past versions of QuickUI markup compiler and runtime files to source countrol in the (unlikely) event a functional old version is ever needed.

---
+++ release/version.js
@@ -0,0 +1,15 @@
+// QuickUI version information
+var quickUIVersion = {
+    "runtime": {
+        "fileName": "quickui-0.9.js", 
+        "version": "0.9"
+    }, 
+    "setup": {
+        "fileName": "QuickUI Setup-0.9.msi", 
+        "version": "0.9"
+    }, 
+    "zip": {
+        "fileName": "QuickUI-0.9.zip", 
+        "version": "0.9"
+    }
+};
